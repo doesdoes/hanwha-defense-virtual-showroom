49e9987f61b5b36fc2911aed900ec80c2de35131
does-vincent <sangoh.lee@does.kr>
2022-06-25 04:49:17 +0900

    Update assets path

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'production',
   entry: {
     _webgl: './src/webgl/_webgl.js',
+    app: [
+      './src/app.js',
+      "./src/scss/app.scss",
+    ]
   },
   output: {
     filename: '[name].js',
@@ -12,4 +17,39 @@ module.exports = {
   performance : {
     hints : false
   },
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+              loader: 'css-loader', 
+              options: {
+                  sourceMap: true, 
+                  url: false
+              }
+          },
+          {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true,
+              }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: (pathData) => {
+          let res;
+          res = './assets/css/[name].css';
+          return res;
+      },
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
+  ],
 }
