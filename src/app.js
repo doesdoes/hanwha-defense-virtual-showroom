cf7d8816c2c5e6911078c32d5f0e44844c7b3e15
does-vincent <sangoh.lee@does.kr>
2022-06-25 02:25:39 +0900

    Add UI #1

--- src/app.js
+++ src/app.js
@@ -1,15 +1,51 @@
+import {gsap} from 'gsap/all';
+
 window.addEventListener('DOMContentLoaded', async (event) => {
-  document.querySelector('.entry__item--left .btn').addEventListener('click', function() {
-    _WEBGL.setLeftScene();
+  document.querySelectorAll('.entry__item .bttn').forEach(bttn => {
+    bttn.addEventListener('click', function() {
+      const item = this.getAttribute('data-item');
+      switch (item) {
+        case 'k9-high':
+        case 'k9-low':
+        case 'redback-high':
+        case 'redback-low':
+          goToContent()
+          break;
+      
+        default:
+          break;
+      }
+    })
   })
 
-  document.querySelector('.entry__item--right .btn').addEventListener('click', function() {
-    _WEBGL.setRightScene();
+  document.querySelector('.header .btn-back').addEventListener('click', function(e) {
+    goToGate();
+
+    e.preventDefault();
   })
 
-  document.querySelector('.header .btn--back').addEventListener('click', function() {
-    _WEBGL.resetScene();
+  document.querySelector('#sound').addEventListener('click', function() {
+    this.classList.toggle('on');
+
+    // [TODO]
+    if(this.class.classList.contain('on')) {
+
+    } else {
+      
+    }
   })
+
+  function goToContent() {
+    gsap.to('.entry', {autoAlpha: 0})
+    gsap.to('#content-wrapper', {autoAlpha: 1})
+    document.querySelector('.header').setAttribute('data-state', 'showroom')
+  }
+
+  function goToGate() {
+    gsap.to('.entry', {autoAlpha: 1})
+    gsap.to('#content-wrapper', {autoAlpha: 0})
+    document.querySelector('.header').setAttribute('data-state', 'gate')
+  }
 
 
   // [NOTE] WEBGL
