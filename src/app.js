85d00370caf70f7650ba8b6e2fd2df4d7835540b
does-vincent <sangoh.lee@does.kr>
2022-06-25 03:10:31 +0900

    Update indicator

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import {gsap} from 'gsap/all';
 
 window.addEventListener('DOMContentLoaded', async (event) => {
+  setContent()
+
   document.querySelectorAll('.entry__item .bttn').forEach(bttn => {
     bttn.addEventListener('click', function() {
       const item = this.getAttribute('data-item');
@@ -31,20 +33,30 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     if(this.class.classList.contain('on')) {
 
     } else {
-      
+
     }
   })
+
+  function setContent() {
+    gsap.set('.indicator-panel', {autoAlpha: 0, x: 20})
+    gsap.set('.bot', {autoAlpha: 0, x: 20})
+  }
 
   function goToContent() {
     gsap.to('.entry', {autoAlpha: 0})
     gsap.to('#content-wrapper', {autoAlpha: 1})
     document.querySelector('.header').setAttribute('data-state', 'showroom')
+
+    gsap.to('.indicator-panel', {autoAlpha: 1, x: 0, delay: 0.2, duration: 0.7})
+    gsap.to('.bot', {autoAlpha: 1, x: 0, delay: 0.4, duration: 0.6})
   }
 
   function goToGate() {
     gsap.to('.entry', {autoAlpha: 1})
     gsap.to('#content-wrapper', {autoAlpha: 0})
     document.querySelector('.header').setAttribute('data-state', 'gate')
+    gsap.to('.indicator-panel', {autoAlpha: 0, x: 20})
+    gsap.to('.bot', {autoAlpha: 0, x: 20})
   }
 
 
