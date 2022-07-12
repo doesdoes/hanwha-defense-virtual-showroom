bf6c56f3cbe3520260d622e396a9965a741d2e5f
does-vincent <sangoh.lee@does.kr>
2022-07-12 21:46:25 +0900

    Update tex floor

--- src/webgl/interactions.js
+++ src/webgl/interactions.js
@@ -93,77 +93,7 @@ export function setCondition() {
       default:
         break;
     }
-  
-    // if(bg === 'k9a1IndoorBg') {
-    //   document.body.setAttribute('data-state', 'K9A1-SNOW')
-    //   this.setAttribute('data-bg', 'snowBg')
-    //   SCENE.toggleStages(true, 'snowBg')
-    //   SCENE.toggleStages(false, 'k9a1IndoorBg')
-    //   window.isAnim = true
-      
-    //   setHemisphereLightSnow(STATE)
-
-    //   STATE.ANIMATIONS._k9Tank.mixer._actions.forEach(anim => {
-    //     anim.play()
-    //   })
-    //   STATE.ANIMATIONS._SNOW.mixer._actions.forEach(anim => {
-    //     anim.play()
-    //   })
-      
-    // } else {
-    //   document.body.setAttribute('data-state', 'K9A1')
-    //   this.setAttribute('data-bg', 'k9a1IndoorBg')
-    //   SCENE.toggleStages(false, 'snowBg')
-    //   SCENE.toggleStages(true, 'k9a1IndoorBg')
-    //   window.isAnim = false
-  
-    //   setHemisphereLightDefault(STATE)
-
-    //   STATE.ANIMATIONS._k9Tank.mixer._actions.forEach(anim => {
-    //     anim.stop()
-    //   })
-    //   // STATE.ANIMATIONS._SNOW.mixer._actions.forEach(anim => {
-    //   //   anim.stop()
-    //   // })
-    //   STATE.ANIMATIONS._SNOW.mixer.stopAllAction()
-    // }
     e.preventDefault()
   })
   
-  // document.querySelector('#change-condition-redback').addEventListener('click', function(e) {
-  //   this.blur()
-  //   const bg = this.getAttribute('data-bg')
-    
-  //   if(bg === 'redbackIndoorBg') {
-  //     document.body.setAttribute('data-state', 'REDBACK-DESERT')
-  //     this.setAttribute('data-bg', 'desertBg')
-  //     SCENE.toggleStages(true, 'desertBg')
-  //     SCENE.toggleStages(false, 'redbackIndoorBg')
-  //     window.isAnim = true
-      
-  //     setHemisphereLightDesert(STATE)
-
-  //     STATE.ANIMATIONS._REDBACK.mixer._actions.forEach(anim => {
-  //       anim.play()
-  //     })
-  //     STATE.ANIMATIONS._DESERT.mixer._actions.forEach(anim => {
-  //       anim.play()
-  //     })
-      
-  //   } else {
-  //     document.body.setAttribute('data-state', 'REDBACK')
-  //     this.setAttribute('data-bg', 'redbackIndoorBg')
-  //     SCENE.toggleStages(false, 'desertBg')
-  //     SCENE.toggleStages(true, 'redbackIndoorBg')
-  //     window.isAnim = false
-  
-  //     setHemisphereLightDefault(STATE)
-
-  //     STATE.ANIMATIONS._REDBACK.mixer._actions.forEach(anim => {
-  //       anim.stop()
-  //     })
-  //     STATE.ANIMATIONS._DESERT.mixer.stopAllAction()
-  //   }
-  //   e.preventDefault()
-  // })
 }
