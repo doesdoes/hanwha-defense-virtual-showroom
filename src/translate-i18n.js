7c5aedf5942cd1e8809475d5284f2a732e2b2b95
Billy Chan <billy.chan@does.kr>
2023-04-18 16:36:43 +0900

    update POI text and title for KSLV

--- src/translate-i18n.js
+++ src/translate-i18n.js
@@ -36,10 +36,10 @@ import i18next from "i18next"
         },
         "KSLV": {
           "ton75classEngine": "나로호 개발 당시 선행연구로 진행한 30톤급 액체 엔진 기술을 바탕으로 개발을 진행해, 연소불안정 현상의 기술적 한계를 극복하고 지상 연소시험과 시험발사체 발사를 통한 비행성능 시험을 거쳐 세계 7번째로 중대형 액체엔진 개발에 성공하였습니다​",
-          "engineClustering": "75톤급 액체엔진 4기를 묶어 300톤의 추력을 내는 클러스터링(Clustering)방식을 활용하였으며, 200회에 가까운 실제 연소시험을 거쳐 4기의 엔진을 구조적 안정성을 유지하면서 마치 하나의 엔진처럼 동일한 추력을 내는 기술을 구현하였습니다​",
-          "liquidEngineFuelSystem": "한화는 국내 최초로 중. 대형급 액체 발사체 추진공급계를 개발하였습니다​ 균등한 추진제 공급을 통해 안정적인 발사 성능을 구현하며 공급 배관 및 충전 배출/개폐 밸브 포함 1,473종의 구성품으로 구성됩니다",
+          "engineClustering": "75톤급 액체엔진 4기를 묶어 300톤의 추력을 내는 클러스터링(Clustering)방식을 활용하였으며, 4기의 엔진을 구조적 안정성을 유지하면서 마치 하나의 엔진처럼 동일한 추력을 내는 기술 개발에 기여하였습니다.​",
+          "liquidEngineFuelSystem": "항우연과 한화는 국내 최초로 중형급 액체 발사체 추진공급계를 개발에 참여하였습니다. <br><br>균등한 추진제 공급을 통해 안정적인 발사 성능을 구현하며 공급 배관 및 충전 배출/개폐 밸브 등으로 구성됩니다. ",
           "ton7classEngine": "75톤급과 동일하게 가스발생기 사이클의 액체 엔진이며 누리호 3단에 장착됩니다​",
-          "collisionPreventionSystem": "안정적인 단, 위성 분리를 지원하는 구동 및 충돌방지 시스템입니다",
+          "collisionPreventionSystem": "위성을 보호하는 페어링 분리를 제어하는 시스템입니다",
         },
       },
       en: {
@@ -67,11 +67,11 @@ import i18next from "i18next"
           "isuRubberTrack": "ISU and rubber tracks are installed to ensure excellent ride quality and stable maneuverability.",
         },
         "KSLV": {
-          "ton75classEngine": "ENG 75 ton",
-          "engineClustering": "ENG engine",
-          "liquidEngineFuelSystem": "ENG fuel system",
-          "ton7classEngine": "ENG 7 ton",
-          "collisionPreventionSystem": "ENG collision",
+          "ton75classEngine": "나로호 개발 당시 선행연구로 진행한 30톤급 액체 엔진 기술을 바탕으로 개발을 진행해, 연소불안정 현상의 기술적 한계를 극복하고 지상 연소시험과 시험발사체 발사를 통한 비행성능 시험을 거쳐 세계 7번째로 중대형 액체엔진 개발에 성공하였습니다​",
+          "engineClustering": "75톤급 액체엔진 4기를 묶어 300톤의 추력을 내는 클러스터링(Clustering)방식을 활용하였으며, 4기의 엔진을 구조적 안정성을 유지하면서 마치 하나의 엔진처럼 동일한 추력을 내는 기술 개발에 기여하였습니다.​",
+          "liquidEngineFuelSystem": "항우연과 한화는 국내 최초로 중형급 액체 발사체 추진공급계를 개발에 참여하였습니다. <br><br>균등한 추진제 공급을 통해 안정적인 발사 성능을 구현하며 공급 배관 및 충전 배출/개폐 밸브 등으로 구성됩니다. ",
+          "ton7classEngine": "75톤급과 동일하게 가스발생기 사이클의 액체 엔진이며 누리호 3단에 장착됩니다​",
+          "collisionPreventionSystem": "위성을 보호하는 페어링 분리를 제어하는 시스템입니다",
         },
       },
     }
