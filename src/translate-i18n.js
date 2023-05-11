19ed74eb8acc25bf6a65f593d026324a98e238c3
Billy Chan <billy.chan@does.kr>
2023-05-11 15:03:28 +0900

    update translation

--- src/translate-i18n.js
+++ src/translate-i18n.js
@@ -22,11 +22,11 @@ import i18next from "i18next"
         },
         "K9A1": {
           "title": "K9 자주포",
-          "longerFiringRange": "K9 자주포 자주포는 360도 전방위 공격이 가능한 자주포로, K307탄을 활용하여 40km의 화력 지원이 가능하고 NATO 규격탄과의 호환이 가능하도록 개발했습니다. 사격정보를 받은 뒤 기동간 60초 이내, 정지간 30초 이내 사격이 가능합니다.",
-          "highMobility": "K9 자주포 자주포의 강력한 파워팩은 유기압 현수장치와 결합되어, 최대 67km/h 빠른 속도와 높은 사격정확도. 그리고 사격 후의 신속한 진지 이탈이 가능하여 아군의 생존성을 높입니다.",
-          "ammunitionLoader": "K9 자주포는 최대 48발의 탄약 적재 능력을 갖추고 있습니다.<br> 현재 탄약 적재부터 이송, 장전까지 전 과정을 완전 자동화하여 발사속도를 향상시킨 K9A2 자주포가 개발 중에 있습니다.",
-          "automaticControlSystem": "자동 사격통제장치와 GPS를 적용하여 사격정확성과 효율성을 향상하였으며 디지털 지도와 전자식 교범을 탑재하여 운용 편의성을 증대하였습니다. ",
-          "irCamera": "야간 기동성을 향상시키기 위하여 열상형 야간잠망경을 적용했으며 전/후방 카메라 장착을 통해, 감시경계 강화 및 아군의 생존성을 강화하였습니다.",
+          "longerFiringRange": "K9 자주포는 360도 전방위 사격이 가능한 자주포로, K307탄을 활용하여 40km의 화력 지원이 가능하고, NATO 규격탄과의 호환이 가능하며, 최대 6~8발/분, 3발/15초, 기동간 60초 이내 사격이 가능합니다.",
+          "highMobility": "K9 자주포의 강력한 파워팩은 유기압 현수장치와 결합되어, 최대 67km/h의 기동성과사격충격을 완화하여 사격 후 신속한 진지 이탈로 아군의 생존성을 높이고, 사격 정확도를 향상시켰습니다.",
+          "ammunitionLoader": "K9 자주포는 최대 48발의 탄약 적재 능력을 갖 추고 있으며,<br/> 포탄은 적재부터 이송, 장전까지 의 과정을 자동화하여 신속한 화력집중 사격이 가능합니다. ",
+          "automaticControlSystem": "자동 사격통제장치와 GPS를 적용하여 사격정확 성과 운용 효율성을 증대하였으며, 디지털 지도 와 전자식 교범을 탑재하여 운용 편의성을 향상 하였습니다.",
+          "irCamera": "조종수 야간잠만경을 열상형으로 적용하여 야간 기동성을 향상 시켰으며, 전/후방 카메라 장착으 로 기동간 안전을 확보하였습니다.",
         },
         "REDBACK": {
           "title": "레드백",
@@ -38,11 +38,11 @@ import i18next from "i18next"
         },
         "KSLV": {
           "title": "누리호",
-          "ton75classEngine": "나로호 개발 당시 선행연구로 진행한 30톤급 액체 엔진 기술을 바탕으로 개발을 진행해, 연소불안정 현상의 기술적 한계를 극복하고 지상 연소시험과 시험발사체 발사를 통한 비행성능 시험을 거쳐 세계 7번째로 중대형 액체엔진 개발에 성공하였습니다​",
+          "ton75classEngine": "나로호 개발 당시 선행연구로 진행한 30톤급 액체 엔진 기술을 바탕으로 개발을 진행해, 연소불안정 현상의 기술적 한계를 극복하고 지상 연소시험과 시험발사체 발사를 통한 비행성능 시험을 거쳐 세계 7번째로 중대형 액체엔진 개발에 성공하였습니다.​",
           "engineClustering": "75톤급 액체엔진 4기를 묶어 300톤의 추력을 내는 클러스터링(Clustering)방식을 활용하였으며, 4기의 엔진을 구조적 안정성을 유지하면서 마치 하나의 엔진처럼 동일한 추력을 내는 기술 개발에 기여하였습니다.​",
-          "liquidEngineFuelSystem": "항우연과 한화는 국내 최초로 중형급 액체 발사체 추진공급계를 개발에 참여하였습니다. 균등한 추진제 공급을 통해 안정적인 발사 성능을 구현하며 공급 배관 및 충전 배출/개폐 밸브 등으로 구성됩니다. ",
-          "ton7classEngine": "75톤급과 동일하게 가스발생기 사이클의 액체 엔진이며 누리호 3단에 장착됩니다​",
-          "collisionPreventionSystem": "위성을 보호하는 페어링 분리를 제어하는 시스템입니다",
+          "liquidEngineFuelSystem": "항우연과 한화는 국내 최초로 중형급 액체 발사체 추진공급계를 개발에 참여하였습니다. 균등한 추진제 공급을 통해 안정적인 발사 성능을 구현하며 공급 배관 및 충전 배출/개폐 밸브 등으로 구성됩니다.",
+          "ton7classEngine": "75톤급과 동일하게 가스발생기 사이클의 액체 엔진이며 누리호 3단에 장착됩니다.​",
+          "collisionPreventionSystem": "위성을 보호하는 페어링 분리를 제어하는 시스템입니다.",
         },
       },
       en: {
@@ -77,7 +77,7 @@ import i18next from "i18next"
           "engineClustering": "Our development efforts involved utilizing a clustering method to combine four 75-ton liquid engines, generating a total thrust of 300 tons, and we were able to ensure structural stability of the engines while producing the same thrust as a single engine, thereby contributing to technological advancements. ​",
           "liquidEngineFuelSystem": "For the first time in Korea, the Korea Aerospace Research Institute and Hanwha Aerospace jointly developed a propulsion supply system for medium-sized liquid-propellant launch vehicles. This system consists of supply pipes, charging and discharge valves, and enables stable launch performance through uninterrupted fuel supply.",
           "ton7classEngine": "The third stage of the Nuri rocket will be outfitted with a liquid-propellant engine featuring a gas generator cycle identical to that of the 75-ton engine.",
-          "collisionPreventionSystem": "This system is designed to control fairing separation to safeguard the satellite. ",
+          "collisionPreventionSystem": "This system is designed to control fairing separation to safeguard the satellite.",
         },
       },
     }
